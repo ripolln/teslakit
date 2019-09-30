036c59fec2ef6757107ea1e061a18b967726c6d8
Alba Cid <albacid@hidropc1.caminos.unican.es>
2019-09-30 09:52:49 +0200

    nb guam

--- scripts/extract_SLP_CFSR.py
+++ scripts/extract_SLP_CFSR.py
@@ -1,6 +1,6 @@
 
 from teslakit.io.cfs import ReadSLP
-
+import os
 
 
 p_db = '/Volumes/CLIMUC/DATOS/CFS/prmsl'
@@ -10,7 +10,10 @@ lon1 = 115
 lon2 = 279
 resample = 4 # coge uno de cada 4 datos
 
-p_save = '/Users/albacid/Projects/TeslaKit_projects/sites/GUAM/ESTELA'
+rutout = '/Users/albacid/Projects/TeslaKit_projects/sites/GUAM/ESTELA/'
+file_name ='SLP_Guam_{0:.1f}_{1:.1f}_{2:.1f}_{3:.1f}.nc'.format(lon1, lon2, lat1, lat2)
 
+p_save = os.path.join(rutout,file_name)
+print(p_save)
 
 slp = ReadSLP(p_db, lat1, lat2, lon1, lon2, resample, p_save)
